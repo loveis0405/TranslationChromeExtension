6c1b7bb081f341f8f7613de4e61304227b6f876a
nomin.myoung <nomin.myoung@lge.com>
2019-06-26 15:55:19 +0900

    update

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var apiKey = "AIzaSyD2x6swAJBO4ZmHzD8BGqQoG-K4pf8A590"; // 요기에 키를 넣어야 동작해여
+var apiKey = ""; // 요기에 키를 넣어야 동작해여
 var targetLang = "ko";
 
 chrome.runtime.onMessage.addListener(function (msg) {
@@ -39,7 +39,7 @@ function requestTranslate(text) {
     var url = "https://translation.googleapis.com/language/translate/v2?key=" + apiKey + "&q="+text+"&format=text&target=" + targetLang;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, false);
-    xhr.send();  
+    xhr.send();
     var result = JSON.parse(xhr.responseText);
     if (result.error == undefined) {
         var resultText = result.data.translations[0].translatedText
